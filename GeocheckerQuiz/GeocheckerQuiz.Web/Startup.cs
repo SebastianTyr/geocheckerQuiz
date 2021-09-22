c8621f76078c3def550e7c801e5a058b7bac1769
SebastianTyr <sebastian.tyralski@microsoft.wsei.edu.pl>
2021-09-22 22:07:58 +0200

    New Project Initial

--- GeocheckerQuiz/GeocheckerQuiz.Web/Startup.cs
+++ GeocheckerQuiz/GeocheckerQuiz.Web/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,8 +38,11 @@ namespace GeocheckerQuiz.Web
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
