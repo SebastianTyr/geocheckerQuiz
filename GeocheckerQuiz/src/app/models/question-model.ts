355b2061b36b1c3ed115cee492a8c28d1675b6e2
SebastianTyr <sebastian.tyralski@microsoft.wsei.edu.pl>
2021-10-02 01:22:36 +0200

    add absed quiz model

---
+++ GeocheckerQuiz/src/app/models/question-model.ts
@@ -0,0 +1,9 @@
+export class QuestionModel {
+    "id": number;
+    "text": string
+    "answer_A": string;
+    "answer_B": string;
+    "answer_C": string;
+    "answer_D": string;
+    "correct_answer": string;
+}
